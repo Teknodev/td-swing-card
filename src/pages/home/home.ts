89e30c1d61b0f1c09853fed983bee551e1085aa1
Şahin Yort <thesayyn@Sahins-MacBook-Pro.local>
2017-12-20 18:34:26 +0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SwingStackComponent, SwingCardComponent } from 'angular2-swing'
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild(SwingStackComponent) stack : SwingStackComponent;
+  @ViewChildren(SwingCardComponent) cards : QueryList<SwingCardComponent>;
+
+  cardsDummy: Array<any> = [
+    { 
+      name: 'Nancy Ahmad',
+      picture : '',
+      position : 'Human Resources Manager',
+      description :  "Guiatr Learning",
+    },
+    { 
+      name: 'Nancy Ahmad',
+      picture : '',
+      position : 'Human Resources Manager',
+      description :  "Guiatr Learning",
+    },
+    { 
+      name: 'Nancy Ahmad',
+      picture : '',
+      position : 'Human Resources Manager',
+      description :  "Guiatr Learning",
+    },
+        { 
+      name: 'Nancy Ahmad',
+      picture : '',
+      position : 'Human Resources Manager',
+      description :  "Guiatr Learning",
+    }
+  ]
   constructor(public navCtrl: NavController) {
+
+  }
+
+  voteUp(operation : boolean)
+  {
+    if(operation)
+    {
+
+    }
+    console.log(this.cards.last.getCard());
 
   }
 
